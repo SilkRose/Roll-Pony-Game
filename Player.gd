f6b5b07e4b2354094ea8153562991c95221e0cdc
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-06 20:04:24 -0500

    Improve movement code

--- Player.gd
+++ Player.gd
@@ -3,15 +3,13 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 2
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -2
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 2
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -2
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		velocity = Vector2.ZERO
+	
 	move_and_collide(velocity)
