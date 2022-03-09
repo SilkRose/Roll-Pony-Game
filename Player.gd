be2c1b7b6693c7c5bda2c641b4efec4bc5b1baba
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-08 23:13:30 -0500

    Make player sprite change direction with movement

--- Player.gd
+++ Player.gd
@@ -4,12 +4,19 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 var velocity = Vector2.ZERO
+onready var sprite = $Player
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	var direction = sign(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
 	
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
