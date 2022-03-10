d967d154cce8c3ebe148d6202d193e18797b83a6
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-10 11:46:22 -0500

    Remove unused code

--- Player.gd
+++ Player.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 const ACCELERATION = 800
 const MAX_SPEED = 75
-#const FRICTION = 800
 var velocity = Vector2.ZERO
 var gravity = 1000
 var max_fall_speed = 100
@@ -14,7 +13,6 @@ onready var sprite = $Playerx32
 func _physics_process(delta):
 	var direction = sign(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
 	var jumping = Input.is_action_pressed("jump")
-	#var on_ground = velocity.y == 0
 	if is_on_floor() && jumping:
 		velocity.y = jump_force
 		local_hold = jump_hold
@@ -36,9 +34,4 @@ func _physics_process(delta):
 	velocity.x = int(move_toward(velocity.x, MAX_SPEED * direction, ACCELERATION * delta))
 	velocity.y = int(move_toward(velocity.y, max_fall_speed, gravity * delta))
 
-	#if input_vector != Vector2.ZERO:
-		#velocity.x = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	#else:
-		#velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
