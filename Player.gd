05ba0cce85b95e638fe8f5b9eef1d63c94a53fb3
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-12 02:36:32 -0500

    Improve movement code

--- Player.gd
+++ Player.gd
@@ -1,40 +1,45 @@
 extends KinematicBody2D
 
-const ACCELERATION = 800
-const MAX_SPEED = 75
-var velocity = Vector2.ZERO
-var gravity = 1000
-var max_fall_speed = 100
-var jump_force = -120
-var jump_hold = 0.25
-var local_hold = 0
+export var move_speed = 80
+export var acceleration = 800
+
+var velocity := Vector2.ZERO
+
+export var jump_height : float
+export var jump_time_to_peak : float
+export var jump_time_to_descent : float
 onready var sprite = $Playerx32
 onready var collision = $CollisionPolygon2D
+onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 func _physics_process(delta):
-	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var jumping = Input.is_action_pressed("jump")
-	if is_on_floor() && jumping:
-		velocity.y = jump_force
-		local_hold = jump_hold
-		velocity.y = int(move_toward(velocity.y, max_fall_speed, gravity * delta))
-	elif local_hold > 0:
-		if jumping:
-			velocity.y = jump_force
-		else:
-			local_hold = 0
+	velocity.y += get_gravity() * delta
+	velocity.x = move_toward(velocity.x, move_speed * get_input_velocity(), acceleration * delta)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump()
 	
-	local_hold -= delta
-	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func get_gravity() -> float:
+	if !is_on_floor() and !is_on_wall():
+		return jump_gravity if velocity.y < 0.0 else fall_gravity
+	else:
+		 return 0.0
+
+func jump():
+	velocity.y = jump_velocity
+
+func get_input_velocity() -> float:
+	var horizontal = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	animate_sprite(horizontal)
+	return horizontal
+
+func animate_sprite(direction):
 	if direction > 0:
 		sprite.flip_h = false
 		collision.scale.x = 1
 	elif direction < 0:
 		sprite.flip_h = true
 		collision.scale.x = -1
-	
-	velocity.x = int(move_toward(velocity.x, MAX_SPEED * direction, ACCELERATION * delta))
-	if !is_on_floor() && !is_on_wall():
-		velocity.y = int(move_toward(velocity.y, max_fall_speed, gravity * delta))
-
-	velocity = move_and_slide(velocity, Vector2(0, -1))
