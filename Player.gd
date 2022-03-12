e7b3556eff01c5625a58c9dd9b4b75a2111c97e2
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-11 22:56:18 -0500

    Make collision an onready var

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ var jump_force = -120
 var jump_hold = 0.25
 var local_hold = 0
 onready var sprite = $Playerx32
+onready var collision = $CollisionPolygon2D
 
 func _physics_process(delta):
 	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -27,10 +28,10 @@ func _physics_process(delta):
 	
 	if direction > 0:
 		sprite.flip_h = false
-		$CollisionPolygon2D.scale.x = 1
+		collision.scale.x = 1
 	elif direction < 0:
 		sprite.flip_h = true
-		$CollisionPolygon2D.scale.x = -1
+		collision.scale.x = -1
 	
 	velocity.x = int(move_toward(velocity.x, MAX_SPEED * direction, ACCELERATION * delta))
 	if !is_on_floor() && !is_on_wall():
