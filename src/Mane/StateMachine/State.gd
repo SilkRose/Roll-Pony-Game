adc780243b109f8c95362d625990746c3b811eed
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-03-14 09:34:50 -0400

    Add statemachine and state basic scripts

---
+++ src/Mane/StateMachine/State.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name State, "res://assets/icons/state.svg"
+"""
+State interface to use in Hierarchical State Machines.
+The lowest leaf tries to handle callbacks, and if it can't, it delegates the work to its parent.
+It's up to the user to call the parent state's functions, e.g. `get_parent().physics_process(delta)`
+Use State as a child of a StateMachine node.
+"""
+
+
+onready var _state_machine: = _get_state_machine(self)
+
+
+func unhandled_input(event: InputEvent) -> void:
+	pass
+
+
+func physics_process(delta: float) -> void:
+	pass
+
+
+func enter(msg: Dictionary = {}) -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func _get_state_machine(node: Node) -> Node:
+	if node != null and not node.is_in_group("state_machine"):
+		return _get_state_machine(node.get_parent())
+	return node
